047acb15ce8ba127f25b11f0be1752941cc31b46
Ingvildb1 <111681292+Ingvildb1@users.noreply.github.com>
2023-10-15 21:48:55 +0200

    checkout alert btn

--- src/components/pages/CheckoutPage.js
+++ src/components/pages/CheckoutPage.js
@@ -3,24 +3,32 @@ import { Link } from "react-router-dom";
 import { CartContext } from "../../App";
 import './../../App.css';
 
-
 const CheckoutPage = () => {
   const { setCart } = useContext(CartContext);
 
   const handleCheckout = () => {
+    // Clear the cart
     setCart([]);
+
+    // Show a popup alert
+    window.alert("Thank you for your purchase!");
   };
 
   return (
     <div>
       <h2>Checkout</h2>
       <p>Almost done! Complete the check out by confirming</p>
-      <button className="checkoutbtn" onClick={handleCheckout}>Complete Checkout</button>
-    <div className="homeLink">
-    <Link to="/" className="checkoutLink">Or go back to store</Link>
-    </div>
+      <button className="checkoutbtn" onClick={handleCheckout}>
+        Complete Checkout
+      </button>
+      <div className="homeLink">
+        <Link to="/" className="checkoutLink">
+          Or go back to store
+        </Link>
+      </div>
     </div>
   );
 };
 
 export default CheckoutPage;
+
